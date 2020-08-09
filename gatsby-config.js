f29a7aeab1abb0ec78085417a431004152153cf9
Sean <seanmcleod93@gmail.com>
2020-08-09 17:40:59 +1000

    Add blog functionality and some posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,21 @@ module.exports = {
       'A site we built together during a full-day Frontend Masters Gatsby workshop!',
   },
   plugins: ['gatsby-plugin-emotion',
-    'gatsby-plugin-react-helmet'],
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      }
+    },
+  ],
 };
