4524aebd2531438a32f724d038ea279b53ab5237
Sean <seanmcleod93@gmail.com>
2020-08-10 20:53:16 +1000

    Add gatsby-image functionality and pull in data from Instagram

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
 
@@ -8,14 +9,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Melbourne!</p>
-      <Link to="/about">Learn about me &rarr;</Link>
-      
+  <>
+    <Hero />
+    <Layout>
       <h2>Read my blog</h2>
       {posts.map(post => (
         <PostPreview key={post.slug} post={post} />
       ))}
-  </Layout>
+      <Insta />
+    </Layout>
+  </>
 );}
